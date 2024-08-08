dd5be9423dc9c47f7dc84bffb8e435228c01b850
BallansByte <maballans@gmail.com>
2024-08-08 21:57:07 +0000

    Tasks

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- function SafeDiv takes 2 arguments: a, INT, b, INT
+-- returns a / b or 0 if b == 0
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER ;
